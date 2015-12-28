9b6b943d5bfe8572ef7e095f446a80fe565e6a48
schasse <schasse@mail.tu-berlin.de>
2015-12-28 20:45:26 +0100

    adjust specs

--- spec/pocket_calendar/pdf_creator_spec.rb
+++ spec/pocket_calendar/pdf_creator_spec.rb
@@ -7,12 +7,11 @@ describe PocketCalendar::PdfCreator do
   let(:svg) { File.read PocketCalendar::TimetableRenderer::TEMPLATE_PATH }
 
   describe '#create_pdf' do
-    let(:generated_pdf) do
-      File.expand_path '../../../calendar.pdf', __FILE__
-    end
+    let(:generated_pdf) { 'tmp/calendar.pdf' }
     let(:page_count) { Prawn::Document.new(template: generated_pdf).page_count }
     before do
-      config.output = 'calendar.pdf'
+      Dir.mkdir 'tmp' unless File.exist? 'tmp'
+      config.output = 'tmp/calendar.pdf'
       creator.create_pdf
     end
     after { File.delete generated_pdf }
